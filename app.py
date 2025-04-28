247e5962533439a6554f0ed2c91380db77728a72
DoanHieu1308 <hieudoan.13082003@gmail.com>
2025-04-29 00:16:18 +0700

    v9

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from routes.ai_speed_to_text_routes import summary_routes
 
@@ -5,4 +6,5 @@ app = Flask(__name__)
 app.register_blueprint(summary_routes)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    port = int(os.environ.get('PORT', 5000))  
+    app.run(host="0.0.0.0", port=port, debug=True)
